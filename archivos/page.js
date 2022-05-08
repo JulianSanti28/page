16789c3ad29a3e097da464b77b94e1816434c085
Julián Santiago Martinez Trullo <juliansmartinez@unicauca.edu.co>
2022-05-08 15:58:11 -0500

    Add slide image in Aficiones template

--- archivos/page.js
+++ archivos/page.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    getImageSlide()
+
     let caracteristica = document.getElementById('caracteristica');
     let array = ["Desarrollador", "Disciplinado ", "Autodidácta", "Estudiante", ]
     let i = 0
@@ -10,5 +12,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
         caracteristica.innerHTML = array[i]
         i++
    }, 1500  )
+
+   var myIndex = 0;
     
 })
+var myIndex = 0;
+function getImageSlide(){
+    let images = document.getElementsByClassName('image-slide')
+    for(let i = 0; i<images.length;i++){
+        images[i].style.display = 'none'
+    }
+    myIndex++;
+    if (myIndex > images.length) {myIndex = 1}    
+    images[myIndex-1].style.display = "block";  
+    setTimeout(getImageSlide, 2500); 
+}
+
+
